8dec710ca66d2c47990a458b9691d7a64db0e6f8
Piyush Saravagi <saravagipiyush@gmail.com>
2019-07-08 00:03:52 -0400

    Added WCF Services

---
+++ WCF Service/AgeCalculatorHostClass/AgeCalculatorHostClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculatorHostClass
+{
+    class AgeCalculatorHostClass
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(AgeCalculatorService.AgeCalculatorServiceClass)))
+            {
+                host.Open();
+                Console.WriteLine("Host started the service");
+                Console.ReadKey();
+            }
+        }
+    }
+}
